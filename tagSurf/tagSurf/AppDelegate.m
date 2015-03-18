ecf0812e1a639d37da83b256068a8e9c1c9322a7
VirtualPaul <VirtualPaul@Gmail.com>
2015-03-17 18:02:30 -0700

    Add handling of push notifications received while app is active

--- tagSurf/tagSurf/AppDelegate.m
+++ tagSurf/tagSurf/AppDelegate.m
@@ -10,6 +10,7 @@
 #import "UAConfig.h"
 #import "UAirship.h"
 #import "UAPush.h"
+#import "AGPushNoteView.h"
 
 
 @implementation AppDelegate
@@ -26,6 +27,27 @@
                                              UIUserNotificationTypeBadge);
     
     return YES;
+}
+
+-(void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //self.textView.text = [userInfo description];
+    // We can determine whether an application is launched as a result of the user tapping the action
+    // button or whether the notification was delivered to the already-running application by examining
+    // the application state.
+    
+    if (application.applicationState == UIApplicationStateActive)
+    {
+        // Nothing to do if applicationState is Inactive, the iOS already displayed an alert view.
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Did receive a Remote Notification" message:[NSString stringWithFormat:@"Your App name received this notification while it was running:\n%@",[[userInfo objectForKey:@"aps"] objectForKey:@"alert"]]delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+//        
+//        [alertView show];
+        NSString *message = [[userInfo objectForKey:@"aps"] objectForKey:@"alert"];
+        if ([message length] != 0)
+            [AGPushNoteView showWithNotificationMessage:message];
+        
+        application.applicationIconBadgeNumber = [[[userInfo objectForKey:@"aps"] objectForKey:@"badge"] integerValue];
+    }    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
