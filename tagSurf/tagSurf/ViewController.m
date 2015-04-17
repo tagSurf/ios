1800d0d26b875c4c090b0ed86fbebc7649479956
VirtualPaul <VirtualPaul@Gmail.com>
2015-04-17 01:05:30 -0700

    Add native Facebook login support for iOS app

--- tagSurf/tagSurf/ViewController.m
+++ tagSurf/tagSurf/ViewController.m
@@ -10,6 +10,8 @@
 #import <URXSearch/URXSearchResult.h>
 #import "UAPush.h"
 #import <sys/utsname.h>
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
 @interface ViewController ()
     
@@ -32,7 +34,15 @@ machineName()
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSURL *url = [NSURL URLWithString:@"http://192.168.1.224:3000/share/funny/0"];
+    
+    [FBSDKProfile enableUpdatesOnAccessTokenChange:YES];
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:FBSDKProfileDidChangeNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        //do whatever you want
+        
+    }];
+    
+    NSURL *url = [NSURL URLWithString:@"http://staging.tagsurf.co/share/funny/0"];
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     self.webView.scalesPageToFit = YES;
     self.webView.delegate = self;
@@ -53,7 +63,7 @@ machineName()
 
 
     if(navigationType == 0 || (navigationType == 5 && !([requestedURL rangeOfString:@"push" options:NSCaseInsensitiveSearch|NSRegularExpressionSearch].location == NSNotFound))) {
-        if(!([requestedURL rangeOfString:@"192.168" options:NSCaseInsensitiveSearch].location == NSNotFound)) {
+        if(!([requestedURL rangeOfString:@"tagsurf" options:NSCaseInsensitiveSearch].location == NSNotFound)) {
             if(!([requestedURL rangeOfString:@"push-enable" options:NSCaseInsensitiveSearch|NSRegularExpressionSearch].location == NSNotFound)) {
                 
                 NSString *user_id = [[requestedURL componentsSeparatedByString:@"/"] objectAtIndex:4];
@@ -99,6 +109,63 @@ machineName()
 //            [application openURL:request.URL];
             return NO;
         }
+    }
+    else if(!([requestedURL rangeOfString:@"auth" options:NSCaseInsensitiveSearch].location == NSNotFound) && !([requestedURL rangeOfString:@"facebook" options:NSCaseInsensitiveSearch].location == NSNotFound)) {
+        NSLog(@"URL Request: %@", requestedURL);
+
+        FBSDKLoginManager *login = [[FBSDKLoginManager alloc] init];
+        [login logInWithReadPermissions:@[@"email",@"public_profile",@"user_friends"] handler:^(FBSDKLoginManagerLoginResult *result, NSError *error) {
+            if (error) {
+                // Process error
+            } else if (result.isCancelled) {
+                // Handle cancellations
+            } else {
+                // If you ask for multiple permissions at once, you
+                // should check if specific permissions missing
+                if ([result.grantedPermissions containsObject:@"email"]) {
+                    // Do work
+                    [self.webView stopLoading];
+                    if ([FBSDKAccessToken currentAccessToken]) {
+                        [[[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil]
+                         startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+                             if (!error) {
+                                 
+                                 NSString *first_name = [result objectForKey:@"first_name"];
+                                 NSString *last_name = [result objectForKey:@"last_name"];
+                                 NSString *email = [result objectForKey:@"email"];
+                                 NSNumber *fbID = [result objectForKey:@"id"];
+                                 NSString *locale = [result objectForKey:@"locale"];
+                                 NSString *gender = [result objectForKey:@"gender"];
+                                 NSString *token = [FBSDKAccessToken currentAccessToken].tokenString;
+                                 
+                                 NSLog(@"location =%@", locale);
+                                 
+                                 NSString *post = [NSString stringWithFormat:@"uid=%@&email=%@&first_name=%@&last_name=%@&facebook_auth_token=%@&gender=%@&location=%@", fbID, email, first_name, last_name, token,  gender, locale];
+                                 
+                                 NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+                                 
+                                 NSString *postLength = [NSString stringWithFormat:@"%lu",[postData length]];
+                                 
+                                 NSMutableURLRequest *postRequest = [[NSMutableURLRequest alloc] init];
+                                 
+                                 [postRequest setURL:[NSURL URLWithString:@"http://staging.tagsurf.co/authentication/from-native"]];
+                                 [postRequest setHTTPMethod:@"POST"];
+                                 [postRequest setValue:postLength forHTTPHeaderField:@"Content-Length"];
+                                 [postRequest setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+                                 [postRequest setHTTPBody:postData];
+                                 
+                                 [self.webView loadRequest:postRequest];
+                                 
+//                                 NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:postRequest delegate:self];
+                             }
+                         }];
+                        
+                    }
+
+                }
+            }
+        }];
+        return NO;
     }
     else return YES;
 }
